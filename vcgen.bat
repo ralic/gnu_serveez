4d22ccc8334788d2a5772ac8d9a1f836dac2f68b
Stefan Jahn <stefan@lkcc.org>
2001-01-12 15:33:42 +0000

    *** empty log message ***

---
+++ vcgen.bat
@@ -0,0 +1,6 @@
+@set SIZZLEPATH=..\sizzle
+copy config.h.serveez config.h
+copy version.h.serveez version.h
+copy config.h.sizzle %SIZZLEPATH%\config.h
+copy version.h.sizzle %SIZZLEPATH%\version.h
+copy libsizzle.dsp %SIZZLEPATH%\libsizzle
