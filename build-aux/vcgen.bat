1967b7195d932328e91e4c14b129282f21a2afe6
Thien-Thi Nguyen <ttn@gnuvola.org>
2011-02-13 01:12:40 +0100

    [build int] Move config.h, version.h into src/.
    * configure.ac (AC_CONFIG_HEADERS): Move config.h into src/.
    (AC_CONFIG_FILES): Likewise for version.h.
    * Makefile.am (EXTRA_DIST): Remove
    config.h.serveez, version.h.serveez.
    (config.h.serveez, version.h.serveez): Delete targets.
    * build-aux/vcgen.bat: Update locations.
    * version.h.in: Delete file.
    * .gitignore: Update; nfc.
    
    * src/version.h.in: New file.
    * src/Makefile.am (EXTRA_DIST): Add config.h.serveez,
    version.h.serveez.
    (config.h.serveez, version.h.serveez): New targets.

--- build-aux/vcgen.bat
+++ build-aux/vcgen.bat
@@ -1,5 +1,5 @@
 @set GUILEPATH=..\guile
-copy config.h.serveez config.h
-copy version.h.serveez version.h
+copy src\config.h.serveez src\config.h
+copy src\version.h.serveez src\version.h
 copy src\svzconfig.h.serveez src\svzconfig.h
 copy src\libserveez\svzpath.h.serveez src\libserveez\svzpath.h
